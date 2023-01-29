e84f6c1966953dc4c1cd827e13c0d46a3679a8eb
한동희 / 서비스개발담당 WEB 개발팀 <oper0116@ssg.com>
2023-01-29 22:11:31 +0900

    feat: 좌측 탭 이동

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,12 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var favorites = <WordPair>[];
 
   void getNext() {
     current = WordPair.random();
     notifyListeners();
   }
-
-  var favorites = <WordPair>[];
 
   void toggleFavorite() {
     if (favorites.contains(current)) {
@@ -43,7 +42,58 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return Scaffold(
+      body: Row(children: [
+        SafeArea(
+            child: NavigationRail(
+          extended: false,
+          destinations: [
+            NavigationRailDestination(
+                icon: Icon(Icons.home), label: Text('Home')),
+            NavigationRailDestination(
+                icon: Icon(Icons.favorite), label: Text('Favorites'))
+          ],
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (value) => {
+            setState(() {
+              selectedIndex = value;
+            })
+          },
+        )),
+        Expanded(
+          child: Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: page,
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
